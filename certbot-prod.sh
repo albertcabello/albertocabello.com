695d6d03e42374bd39b4f43985aad881ab9dafb1
albertcabello <albert.a.cabello@gmail.com>
2021-01-07 18:31:25 +0000

    Changed the certbot scripts to help with renewing certs

--- certbot-prod.sh
+++ certbot-prod.sh
@@ -1,1 +1,9 @@
-docker run -it --rm -v /root/albertocabello.com/sslcerts/:/etc/letsencrypt/ -v /root/albertocabello.com/letsencrypt-artifacts/:/var/lib/lets encrypt/ -v /root/albertocabello.com/websites/ssl-setup/:/data/letsencrypt/ -v /root/albertocabello.comletsencrypt-artifacts/logs/:/var/log/letsencrypt certbot/certbot certonly --webroot --email  albert.a.cabello@gmail.com --agree-tos --no-eff-email --webroot-path=/data/letsencrypt -d bitwarden.albertocabello.com -d www.bitwarden.albertocabello.com
+# This generates a certificate BUT YOU MUST MOVE THE BITWARDEN SITE TO BECOME THE DEFAULT SSL-SITE
+# rename bitwarden.albertocabello.com.conf to bitwarden.albertocabello.com and ssl-site to ssl-site.conf
+# restart docker container and run this script
+# undo the rename and should have the new scripts
+docker run --rm -it --name certbot -v "/root/albertocabello.com/sslcerts:/etc/letsencrypt" -v "/root/albertocabello.com/letsencrypt-artifacts:/var/lib/letsencrypt" -v  "/root/albertocabello.com/websites:/data/letsencrypt" -v "/root/albertocabello.com/letsencrypt-artifacts/logs:/var/log/letsencrypt" certbot/certbot certonly --webroot -w /data/letsencrypt/ssl-setup -d bitwarden.albertocabello.com -d www.bitwarden.albertocabello.com -w /data/letsencrypt/albertocabello.com -d albertocabello.com -d www.albertocabello.com
+
+
+# This lists the certificates generated
+docker run --rm -it --name certbot -v "/root/albertocabello.com/sslcerts:/etc/letsencrypt" -v "/root/albertocabello.com/letsencrypt-artifacts:/var/lib/letsencrypt" -v  "/root/albertocabello.com/websites:/data/letsencrypt" -v "/root/albertocabello.com/letsencrypt-artifacts/logs:/var/log/letsencrypt" certbot/certbot certificates
--- certbot-renew.sh
+++ certbot-renew.sh
@@ -1,1 +1,5 @@
-docker run -it --rm --name certbot -v /root/albertocabello.com/sslcerts:/etc/letsencrypt -v /root/albertocabello.com/letsencrypt-artifacts/lib:/var/lib/letsencrypt -v /root/albertocabello.com/websites/ssl-setup:/data/letsencrypt -v /root/albertocabello.com/letsencrypt-artifacts/logs:/var/log/letsencrypt certbot/certbot renew --webroot -w /data/letsencrypt --quiet && docker kill --signal=HUP reverseproxy
+# This renews a certificate
+docker run --rm -it --name certbot -v "/root/albertocabello.com/sslcerts:/etc/letsencrypt" -v "/root/albertocabello.com/letsencrypt-artifacts:/var/lib/letsencrypt" -v  "/root/albertocabello.com/websites:/data/letsencrypt" -v "/root/albertocabello.com/letsencrypt-artifacts/logs:/var/log/letsencrypt" certbot/certbot renew --dry-run
+
+# This lists the certificates generated
+docker run --rm -it --name certbot -v "/root/albertocabello.com/sslcerts:/etc/letsencrypt" -v "/root/albertocabello.com/letsencrypt-artifacts:/var/lib/letsencrypt" -v  "/root/albertocabello.com/websites:/data/letsencrypt" -v "/root/albertocabello.com/letsencrypt-artifacts/logs:/var/log/letsencrypt" certbot/certbot certificates
